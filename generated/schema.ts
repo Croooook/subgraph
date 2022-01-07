a32c9b213b95a8f3d3c9fcfd441d162470e02a27
Croooook <rajatgoyal6404@gmail.com>
2022-01-07 22:44:13 +0530

    build the schema, build mapping, run the eventhandler

--- generated/schema.ts
+++ generated/schema.ts
@@ -11,34 +11,36 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
-export class Incentive extends Entity {
+export class IncentiveCreated extends Entity {
   constructor(id: string) {
     super();
     this.set("id", Value.fromString(id));
 
-    this.set("rewardToken", Value.fromString(""));
-    this.set("pool", Value.fromString(""));
+    this.set("rewardToken", Value.fromBytes(Bytes.empty()));
+    this.set("pool", Value.fromBytes(Bytes.empty()));
     this.set("startTime", Value.fromBigInt(BigInt.zero()));
     this.set("endTime", Value.fromBigInt(BigInt.zero()));
-    this.set("address", Value.fromString(""));
+    this.set("refundee", Value.fromBytes(Bytes.empty()));
     this.set("reward", Value.fromBigInt(BigInt.zero()));
   }
 
   save(): void {
     let id = this.get("id");
-    assert(id != null, "Cannot save Incentive entity without an ID");
+    assert(id != null, "Cannot save IncentiveCreated entity without an ID");
     if (id) {
       assert(
         id.kind == ValueKind.STRING,
-        "Cannot save Incentive entity with non-string ID. " +
+        "Cannot save IncentiveCreated entity with non-string ID. " +
           'Considering using .toHex() to convert the "id" to a string.'
       );
-      store.set("Incentive", id.toString(), this);
+      store.set("IncentiveCreated", id.toString(), this);
     }
   }
 
-  static load(id: string): Incentive | null {
-    return changetype<Incentive | null>(store.get("Incentive", id));
+  static load(id: string): IncentiveCreated | null {
+    return changetype<IncentiveCreated | null>(
+      store.get("IncentiveCreated", id)
+    );
   }
 
   get id(): string {
@@ -50,22 +52,22 @@ export class Incentive extends Entity {
     this.set("id", Value.fromString(value));
   }
 
-  get rewardToken(): string {
+  get rewardToken(): Bytes {
     let value = this.get("rewardToken");
-    return value!.toString();
+    return value!.toBytes();
   }
 
-  set rewardToken(value: string) {
-    this.set("rewardToken", Value.fromString(value));
+  set rewardToken(value: Bytes) {
+    this.set("rewardToken", Value.fromBytes(value));
   }
 
-  get pool(): string {
+  get pool(): Bytes {
     let value = this.get("pool");
-    return value!.toString();
+    return value!.toBytes();
   }
 
-  set pool(value: string) {
-    this.set("pool", Value.fromString(value));
+  set pool(value: Bytes) {
+    this.set("pool", Value.fromBytes(value));
   }
 
   get startTime(): BigInt {
@@ -86,13 +88,13 @@ export class Incentive extends Entity {
     this.set("endTime", Value.fromBigInt(value));
   }
 
-  get address(): string {
-    let value = this.get("address");
-    return value!.toString();
+  get refundee(): Bytes {
+    let value = this.get("refundee");
+    return value!.toBytes();
   }
 
-  set address(value: string) {
-    this.set("address", Value.fromString(value));
+  set refundee(value: Bytes) {
+    this.set("refundee", Value.fromBytes(value));
   }
 
   get reward(): BigInt {
--- src/mapping.ts
+++ src/mapping.ts
@@ -7,23 +7,13 @@ import {
   TokenUnstaked as TokenUnstakedEvent
 } from "../generated/Staker/Staker"
 import {
-  DepositTransferred,
+  
   IncentiveCreated,
-  IncentiveEnded,
-  RewardClaimed,
-  TokenStaked,
-  TokenUnstaked
+ 
+  
 } from "../generated/schema"
 
-export function handleDepositTransferred(event: DepositTransferredEvent): void {
-  let entity = new DepositTransferred(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.tokenId = event.params.tokenId
-  entity.oldOwner = event.params.oldOwner
-  entity.newOwner = event.params.newOwner
-  entity.save()
-}
+
 
 export function handleIncentiveCreated(event: IncentiveCreatedEvent): void {
   let entity = new IncentiveCreated(
@@ -38,39 +28,5 @@ export function handleIncentiveCreated(event: IncentiveCreatedEvent): void {
   entity.save()
 }
 
-export function handleIncentiveEnded(event: IncentiveEndedEvent): void {
-  let entity = new IncentiveEnded(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.incentiveId = event.params.incentiveId
-  entity.refund = event.params.refund
-  entity.save()
-}
 
-export function handleRewardClaimed(event: RewardClaimedEvent): void {
-  let entity = new RewardClaimed(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.to = event.params.to
-  entity.reward = event.params.reward
-  entity.save()
-}
 
-export function handleTokenStaked(event: TokenStakedEvent): void {
-  let entity = new TokenStaked(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.tokenId = event.params.tokenId
-  entity.incentiveId = event.params.incentiveId
-  entity.liquidity = event.params.liquidity
-  entity.save()
-}
-
-export function handleTokenUnstaked(event: TokenUnstakedEvent): void {
-  let entity = new TokenUnstaked(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.tokenId = event.params.tokenId
-  entity.incentiveId = event.params.incentiveId
-  entity.save()
-}
