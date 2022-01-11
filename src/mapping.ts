6cec1a88a2975127244106d139d3aab4daf7f5b7
Croooook <rajatgoyal6404@gmail.com>
2022-01-12 03:11:55 +0530

    updated the incentivecreated function

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,11 +1,34 @@
 import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  
-  IncentiveCreated,
-  
+  DepositTransferred as DepositTransferredEvent,
+  IncentiveCreated as IncentiveCreatedEvent,
+  IncentiveEnded as IncentiveEndedEvent,
+  RewardClaimed as RewardClaimedEvent,
+  TokenStaked as TokenStakedEvent,
+  TokenUnstaked as TokenUnstakedEvent
 } from "../generated/uniswapv3staker/uniswapv3staker"
 
+import {
+  
+  IncentiveCreated,
+ 
+  
+} from "../generated/schema"
 
-export function handleIncentiveCreated(event: IncentiveCreated): void {}
+
+
+export function handleIncentiveCreated(event: IncentiveCreatedEvent): void {
+  let entity = new IncentiveCreated(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  )
+  entity.rewardToken = event.params.rewardToken
+  entity.pool = event.params.pool
+  entity.startTime = event.params.startTime
+  entity.endTime = event.params.endTime
+  entity.refundee = event.params.refundee
+  entity.reward = event.params.reward
+  entity.save()
+}
+
 
 
