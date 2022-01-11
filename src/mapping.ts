24f42759336529bdb1e88fe98b4ad079d57a51a9
Croooook <rajatgoyal6404@gmail.com>
2022-01-11 23:17:39 +0530

    wrote the whole new code for polygon

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,32 +1,15 @@
+import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  DepositTransferred as DepositTransferredEvent,
-  IncentiveCreated as IncentiveCreatedEvent,
-  IncentiveEnded as IncentiveEndedEvent,
-  RewardClaimed as RewardClaimedEvent,
-  TokenStaked as TokenStakedEvent,
-  TokenUnstaked as TokenUnstakedEvent
-} from "../generated/Staker/Staker"
-import {
-  
+  uniswapv3staker,
+  DepositTransferred,
   IncentiveCreated,
- 
-  
-} from "../generated/schema"
+  IncentiveEnded,
+  RewardClaimed,
+  TokenStaked,
+  TokenUnstaked
+} from "../generated/uniswapv3staker/uniswapv3staker"
 
 
-
-export function handleIncentiveCreated(event: IncentiveCreatedEvent): void {
-  let entity = new IncentiveCreated(
-    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
-  )
-  entity.rewardToken = event.params.rewardToken
-  entity.pool = event.params.pool
-  entity.startTime = event.params.startTime
-  entity.endTime = event.params.endTime
-  entity.refundee = event.params.refundee
-  entity.reward = event.params.reward
-  entity.save()
-}
-
+export function handleIncentiveCreated(event: IncentiveCreated): void {}
 
 
